4da8aaf1c640040213d37dfe167d8e047f6e90dd
migraine-sudo <2576361468@qq.com>
2021-06-05 06:53:59 -0700

    update

--- vuln.cc
+++ vuln.cc
@@ -24,13 +24,18 @@ bool VulnerableFunction3(uint8_t* data, size_t size, uint8_t* data2)
    {
 	data2[i]=data[i];
 	if(data2[i] == 65) //data[i] = A
-		if(data2[i+1] == 66) //data[i] = B
-			if(data2[i+2] == 67) //data[i] = C
-		   		{	
+	{
+			if(data2[i+1] == 66) //data[i] = B
+			{
+					if(data2[i+2] == 67) //data[i] = C
+		   			{	
 					data[i+3]=10;
 					data[i+4]=10;
 					data[i+5]=10;
-				}
+					}
+			}
+
+	}
    }
 	//free(str);
 	return true;
